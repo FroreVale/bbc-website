2dc8f88e8dce0b3b62a92411845d706d5fa299e9
Daniel Raj <danielraj427@gmail.com>
2025-11-02 19:14:11 +0530

    add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,48 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const reithSerif = localFont({
+  src: [
+    {
+      path: "./fonts/BBCReithSerif_W_Rg.woff2",
+      weight: "400",
+    },
+    {
+      path: "./fonts/BBCReithSerif_W_Md.woff2",
+      weight: "500",
+    },
+  ],
+  variable: "--font-reith-serif",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const reithSans = localFont({
+  src: [
+    {
+      path: "./fonts/BBCReithSans_W_Rg.woff2",
+      weight: "400",
+    },
+    {
+      path: "./fonts/BBCReithSans_W_Md.woff2",
+      weight: "500",
+    },
+    {
+      path: "./fonts/BBCReithSans_W_Bd.woff2",
+      weight: "700",
+    },
+    {
+      path: "./fonts/BBCReithSans_W_ExBd.woff2",
+      weight: "800",
+    },
+  ],
+  variable: "--font-reith-sans",
 });
 
 export const metadata: Metadata = {
-  title: "BBC Home - Breaking News, World News, US News, Sports, Business, Innovation, Climate, Culture, Travel, Video & Audio",
-  description: "Visit BBC for trusted reporting on the latest world and US news, sports, business, climate, innovation, culture and much more.",
+  title:
+    "BBC Home - Breaking News, World News, US News, Sports, Business, Innovation, Climate, Culture, Travel, Video & Audio",
+  description:
+    "Visit BBC for trusted reporting on the latest world and US news, sports, business, climate, innovation, culture and much more.",
 };
 
 export default function RootLayout({
@@ -25,7 +53,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${reithSans.variable} ${reithSerif.variable} antialiased`}
       >
         {children}
       </body>
