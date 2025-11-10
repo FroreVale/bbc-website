7f5501f20ea91d6c572bc6f38e88d209f0c7c859
Daniel Raj <danielraj427@gmail.com>
2025-11-10 23:16:11 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
+'use client'
+
 import SortSearchIcon from "@/app/components/icons/sortSearchIcon";
 import BBCLogo from "@/app/components/icons/logo";
 import Button from "@/app/components/ui/button";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY > 10) {
+        setIsScrolled(true) 
+      } else {
+        setIsScrolled(false)
+      }
+    }; 
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll) 
+  }, [])
+
   return (
     <>
       <header>
-        <div className="flex justify-between items-center px-4 py-3">
+        <div className={`flex justify-between items-center px-4 py-3`}>
           <SortSearchIcon  />
           <BBCLogo className="w-40" />
           <div className="flex gap-5">
