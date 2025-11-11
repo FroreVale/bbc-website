efa1f36b249215e010d98ef0e8c36316abe67df2
Daniel Raj <danielraj427@gmail.com>
2025-11-11 08:52:53 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -22,16 +22,17 @@ export default function Home() {
 
   return (
     <>
-      <header>
-        <div className={`flex justify-between items-center px-4 py-3`}>
-          <SortSearchIcon  />
-          <BBCLogo className="w-40" />
-          <div className="flex gap-5">
+      <header className={`fixed top-0 ${isScrolled ? "h-16" : "h-20"} transition-all ease-in-out duration-500 border-b border-border z-50 w-full`} >
+        <div className={`flex items-center h-full px-4 py-3 transition-all duration-500`}>
+          <SortSearchIcon  className="flex-1 flex justify-start items-center" />
+          <BBCLogo className="flex-1 flex justify-center items-center h-[90%] transition-all duration-500 ease-in-out"/>
+          <div className="flex-1 flex items-center justify-end gap-5">
             <Button className="bg-black text-white">Register</Button>
             <Button>Sign In</Button>
           </div>
         </div>
       </header>
+      <div className="h-[10000]"></div>
     </>
   );
 }
